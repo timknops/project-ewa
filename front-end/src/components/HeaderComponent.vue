f5c736e3b23b1d0723948d0846be07bfd3f45c0a
Julian Kruithof <julian.kruithof@hva.nl>
2023-10-08 19:28:48 +0200

    Incorporated changes suggested by tim

--- front-end/src/components/HeaderComponent.vue
+++ front-end/src/components/HeaderComponent.vue
@@ -1,14 +1,14 @@
 
 <template>
   <header class="bg-white mb-3">
-    <div class="row mx-0 px-3 justify-content-end border-bottom align-items-center">
+    <div class="row mx-0 px-3 py-1 justify-content-end border-bottom align-items-center">
       <div class="col-auto d-flex flex-column text-end">
         <span>Logged in as: <strong>{{activeUser.name}}</strong></span>
         <span>Role: <strong>{{activeUser.role}} </strong></span>
       </div>
     </div>
-    <div class="row mx-0 px-5 py-1 justify-content-start align-items-center">
-      <div class="col-auto color-primary" v-if="$route.meta?.icon">
+    <div class="row mx-0 px-5 py-2 justify-content-start align-items-center">
+      <div class="col-auto color-primary mt-1 bg-body-secondary py-2 rounded-5" v-if="$route.meta?.icon">
         <font-awesome-icon  :icon="$route.meta.icon"></font-awesome-icon>
       </div>
       <h2 class="col-auto fs-4 mb-0 color-primary"> {{displayCurrentPageName}} </h2>
@@ -69,5 +69,8 @@ export default {
 }
 .color-primary {
   color: var(--color-primary);
+}
+header {
+  box-shadow: var(--custom-box-shadow);
 }
 </style>
