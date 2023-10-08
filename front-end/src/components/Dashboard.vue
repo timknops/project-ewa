37ad58abda7f29f5c1c477058f6f29af79784ea7
hanan Ouardi <hanan.ouardi@hva.nl>
2023-10-08 20:41:30 +0200

    dashboard

--- front-end/src/components/Dashboard.vue
+++ front-end/src/components/Dashboard.vue
@@ -7,9 +7,10 @@
     <TableComponent
         :tableWidth="'100%'"
         :boldFirstColumn="true"
-        :amountToDisplay="2"
+        :amountToDisplay="3"
         :tableData="tableData"
         :arrayAmountToDisplay="10"
+
     >
     </TableComponent>
 
@@ -17,11 +18,12 @@
       <!--Forecast-->
       <div class="user-table-overview-left">
         <div class="table-container">
-          <div class="forecast-chart">
-            <canvas ref="myChart" style="width:600px; height:300px" class="my-chart"></canvas>
+<!--          <div v-for="(item, index) in tableData" :key="index" class="forecast-chart">-->
+<!--            <canvas  :ref="'myChart_' + index" style="width:600px; height:300px" class="my-chart"></canvas>-->
+          <canvas  ref="combinedChart" style="width:600px; height:300px" class="my-chart"></canvas>
           </div>
         </div>
-      </div>
+
 
       <!--User information-->
       <TableComponent class="user-table-overview-right"
@@ -57,17 +59,29 @@ export default {
         {
           Name: "enphase",
           Quantity: 9,
+          Expected: 32,
         },
         {
           Name: "Gateway",
           Quantity: 18,
+          Expected: 30,
+        },
+        {
+          Name: "MB 385 (white)",
+          Quantity: 18,
+          Expected: 30,
+        },
+        {
+          Name: "DENIM 405 black",
+          Quantity: 18,
+          Expected: 45,
         },
 
       ],
       userData: [
         {
           Username: "hx",
-          Warehouse: "Solar",
+          Warehouse: "Solar clarity",
         },
       ],
       forecastData: [
@@ -77,12 +91,13 @@ export default {
         },
       ],
 
-      chart: null,
+      chartDataList: [],
+  //    chart: null,
       chartWidth: 200,
       chartHeight: 80,
-      xValues: ["This week", "Expected"],
-      yValues: [55, 49],
-      barColors: ["rgba(91, 46, 24, 1)", "rgba(199, 208, 44, 1)"],
+     // xValues: ["This week", "Expected"],
+      //yValues: [0, 36],
+      barColors: ["rgba(91, 46, 24, 1)"],
 
     };
   },
@@ -94,43 +109,77 @@ export default {
 
   methods: {
     createChart() {
-      if (this.chart) {
-        this.chart.destroy();
-      }
-
-
       const chartWidth = this.chartWidth;
       const chartHeight = this.chartHeight;
 
-      this.chart = new Chart(this.$refs.myChart, {
+      const labels = this.tableData.map((item) => item.Name);
+      const data = this.tableData.map((item) => item.Quantity);
+      const expectedData = this.tableData.map((item) => item.Expected);
+
+      data.push(50);
+
+      const chartData = {
+        labels: labels,
+        datasets: [
+          {
+            label: "Quantity",
+            backgroundColor: this.barColors[0], // Use the first color for Quantity
+            data: data,
+          },
+          {
+            label: "Expected",
+            backgroundColor: "rgba(199, 208, 44, 1)", // Use the specified color for Expected
+            data: expectedData,
+          },
+        ],
+      };
+
+      const chartOptions = {
+        legend: { display: false },
+        title: {
+          display: true,
+          text: "Inventory Chart",
+        },
+        width: chartWidth,
+        height: chartHeight,
+        // responsive: true,
+        // maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+        plugins: {
+          tooltip: {
+            enabled: true,
+          },
+        },
+        elements: {
+          bar: {
+            borderRadius: 30,
+          },
+        },
+      };
+
+      new Chart(this.$refs.combinedChart, {
         type: "bar",
-        data: {
-          labels: this.xValues,
-          datasets: [{
-            backgroundColor: this.barColors,
-            // fillColor: ,
-            data: this.yValues,
-          },
-          ],
-        },
-        options: {
-          legend: {display: false},
-          title: {
-            display: true,
-            text: "World Wine Production 2018",
-          },
-
-          width: chartWidth,
-          height: chartHeight,
-
-        },
+        data: chartData,
+        options: chartOptions,
       });
     },
   },
-  watch: {
-    xValues: "createChart",
-    yValues: "createChart",
-  },
+
+
+
+
+
+
+
+  // watch: {
+  //   xValues: "createChart",
+  //   yValues: "createChart",
+  // },
+
 
 
 };
