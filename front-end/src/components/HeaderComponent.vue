83dd87ec343fd4ae740f374cabb115ea9017644e
Julian Kruithof <julian.kruithof@hva.nl>
2023-10-08 18:04:42 +0200

    Added name display for current page

--- front-end/src/components/HeaderComponent.vue
+++ front-end/src/components/HeaderComponent.vue
@@ -1,17 +1,17 @@
 
 <template>
-  <header class="bg-white">
-    <div class="row justify-content-end">
-      <div class="col-auto d-flex">
-        <p>user</p>
-        <p>role</p>
+  <header class="bg-white mb-3">
+    <div class="row mx-0 px-3 justify-content-end border-bottom align-items-center">
+      <div class="col-auto d-flex flex-column text-end">
+        <span>logged in as: user</span>
+        <span>role</span>
       </div>
     </div>
-    <div class="row">
-      <div class="rounded-5 col-auto bg-gray" v-if="$route.meta.icon">
+    <div class="row mx-0 px-3 py-1 justify-content-start align-items-center">
+      <div class="col-auto color-primary" v-if="$route.meta?.icon">
         <font-awesome-icon  :icon="$route.meta.icon"></font-awesome-icon>
       </div>
-      <h2 class="col-auto fs-3"> {{$route.path}} </h2>
+      <h2 class="col-auto fs-4 mb-0 color-primary"> {{displayCurrentPageName}} </h2>
     </div>
   </header>
 </template>
@@ -21,14 +21,20 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
 export default {
   name: "HeaderComponent",
-  components: {FontAwesomeIcon}
+  components: {FontAwesomeIcon},
+  computed: {
+    displayCurrentPageName() {
+      const pageName = this.$route.matched[0].path
+      return pageName.substring(1,2).toUpperCase().concat(pageName.substring(2))
+    }
+  }
 }
 </script>
 <style scoped>
 .bg-white {
   background-color: var(--color-bg);
 }
-.bg-gray {
-  background-color: var(--bs-gray-100);
+.color-primary {
+  color: var(--color-primary);
 }
 </style>
