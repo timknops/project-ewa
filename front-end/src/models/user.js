0c0620b15a7a2c6f4947b35a1f3cf57cdb345e8f
Nashon Woldai <nashon.woldai@hva.nl>
2023-10-09 14:19:27 +0200

    changed the warehouses name

--- front-end/src/models/user.js
+++ front-end/src/models/user.js
@@ -9,12 +9,12 @@ export const generateUser = (Firstname, Surname, Password, Role, Team, Warehouse
 
 export const users = [
     generateUser("Ben", "van Weg", "********", "Employee", "Team 1", "Solar Sedum"),
-    generateUser("Dijkstra", "van der Berg", "********", "Admin", "Team 2", "Warehouse 2"),
-    generateUser("Hassan", "Bahara", "********", "Employee", "Team 2", "Warehouse 2"),
-    generateUser("Ethan", "Bennett", "********", "Employee", "Team 2", "Warehouse 3"),
-    generateUser("Jada ", "Salinas", "********", "Employee", "Team 3", "Warehouse 4"),
-    generateUser("Shannon ", "Mccall", "********", "Admin", "Team 3", "Warehouse 4"),
-    generateUser("Tiago", "Roman", "********", "Employee", "Team 4", "Warehouse 5"),
-    generateUser("Hugh ", "Key", "********", "Admin", "Team 4", "Warehouse 5"),
-    generateUser("Walter", "White", "********", "Employee", "Team 4", "Warehouse 5"),
+    generateUser("Dijkstra", "van der Berg", "********", "Admin", "Team 2", "Solar Sedum"),
+    generateUser("Hassan", "Bahara", "********", "Employee", "Team 2", "EHES"),
+    generateUser("Ethan", "Bennett", "********", "Employee", "Team 2", "Superzon"),
+    generateUser("Jada ", "Salinas", "********", "Employee", "Team 3", "Solar Sedum"),
+    generateUser("Shannon ", "Mccall", "********", "Admin", "Team 3", "Superzon"),
+    generateUser("Tiago", "Roman", "********", "Employee", "Team 4", "EHES"),
+    generateUser("Hugh ", "Key", "********", "Admin", "Team 4", "The switch"),
+    generateUser("Walter", "White", "********", "Employee", "Team 4", "The switch"),
 ];
