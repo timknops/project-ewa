4c750965ec7c64829cd1194b0b07f0973ecb85e8
Tim Knops <timknops22@gmail.com>
2023-11-15 12:46:45 +0100

    Further work on the add project modal form

--- front-end/src/components/modal/project/AddProjectModal.vue
+++ front-end/src/components/modal/project/AddProjectModal.vue
@@ -46,6 +46,46 @@
         </small>
       </div>
     </div>
+
+    <div class="mb-3 grid row">
+      <div class="col">
+        <label for="team-select" class="form-label fw-bold"
+          >Team <span class="text-danger">*</span></label
+        >
+        <select
+          v-model="modalItem.team"
+          @blur="validateTeam"
+          class="form-select"
+          :class="{ 'border-danger': teamUnselected }"
+          id="team-select"
+          aria-label="team"
+        >
+          <option selected value="">Assign team</option>
+          <option v-for="team in TEAM_OPTIONS" :key="team" :value="team">
+            {{ team }}
+          </option>
+        </select>
+        <small v-if="teamUnselected" class="text-danger"
+          >The team can't be unassigned!</small
+        >
+      </div>
+
+      <div class="col">
+        <label for="status-select" class="form-label fw-bold"
+          >Status <span class="text-danger">*</span></label
+        >
+        <select class="form-select" id="status-select" aria-label="status">
+          <option selected>Assign status</option>
+          <option
+            v-for="status in STATUS_OPTIONS"
+            :key="status"
+            :value="status"
+          >
+            {{ status }}
+          </option>
+        </select>
+      </div>
+    </div>
   </form>
 </template>
 
@@ -69,23 +109,36 @@ export default {
         description: "",
         products: [],
       },
-      hasError: false,
       nameEmpty: false,
       dueDateEmpty: false,
+      teamUnselected: false,
+      TEAM_OPTIONS: ["Team 1", "Team 2", "Team 3"],
+      STATUS_OPTIONS: ["Upcoming", "In Progress", "Completed"],
     };
+  },
+  computed: {
+    hasError() {
+      return this.nameEmpty || this.dueDateEmpty || this.teamUnselected;
+    },
   },
   methods: {
     validateName() {
       this.nameEmpty = this.modalItem.projectName.length === 0;
-      this.hasError = this.modalItem.projectName.length === 0;
     },
 
     validateDueDate() {
       this.dueDateEmpty = this.modalItem.dueDate.length === 0;
-      this.hasError = this.modalItem.dueDate.length === 0;
+    },
+
+    validateTeam() {
+      this.teamUnselected = this.modalItem.team.length === 0;
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+.modal-dialog {
+  min-width: 600px !important;
+}
+</style>
