8ccdcc8e50de0adc1adfbc17dab90177cab96771
Noa de Greef <noa.de.greef@hva.nl>
2023-12-11 20:15:34 +0100

    Small changes to email back-end and resetLogin front-end - Added more comments - fixed the url processing in reset login component - removed some outdated code

--- back-end/src/main/java/nl/solar/app/rest/EmailController.java
+++ back-end/src/main/java/nl/solar/app/rest/EmailController.java
@@ -13,19 +13,28 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
+    /**
+     * Sends an email to the user that request a password reset
+     *
+     * @param givenEmail email received from the front-end
+     * @return an email with the link to reset the password of the user
+     */
     @PostMapping("")
-    public String sendEmail(@RequestBody String givenEmail
+    public String sendResetEmail(@RequestBody String givenEmail
 
-    ){
+    ) {
         Email fullEmail = new Email();
 
+        //split the url into it's individual params
         String[] bodySplitter = givenEmail.split("_", 3);
         String email = bodySplitter[0];
         String name = bodySplitter[1];
         String id = bodySplitter[2];
 
+        //link for the password reset
         String resetLink = "https://front-end-8cvv.onrender.com/#/loginReset/" + email + "_" + name + "_" + id;
 
+        //set email properties
         fullEmail.setReceiver(email);
         fullEmail.setEmailBody("A login request for this email address was requested on " + resetLink);
         fullEmail.setSubject("Password reset request");
