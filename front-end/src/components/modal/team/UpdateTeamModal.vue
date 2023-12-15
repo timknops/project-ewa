7095f60e73eb6bddd5ef288ece125b3fb0ad19c9
Nashon Woldai <nashon.woldai@hva.nl>
2023-12-15 19:43:43 +0100

    adjusted the team edit

--- front-end/src/components/modal/team/UpdateTeamModal.vue
+++ front-end/src/components/modal/team/UpdateTeamModal.vue
@@ -2,12 +2,14 @@
   <form>
     <div class="mb-3">
       <label for="team-name" class="form-label fw-bold">Team name</label>
-      <input id="team-name"
-             type="text"
-             class="form-control"
-             :class="{'border-danger': teamEmpty}"
-             v-model.lazy.trim="modalItem.team"
-             @blur="validateTeam">
+      <input
+          id="team-name"
+          type="text"
+          class="form-control"
+          :class="{'border-danger': teamEmpty}"
+          v-model.lazy.trim="modalItem.team"
+          @blur="validateTeam"
+      />
       <p v-if="teamEmpty" class="text-danger"> The team name can't be empty!</p>
     </div>
     <div class="mb-3">
@@ -19,9 +21,9 @@
           v-model="modalItem.warehouse"
           @blur="validateWarehouse"
       >
-        <option value="" disabled selected>Select Warehouse</option>
-        <option v-for="(warehouse, index) in warehouseOptions" :key="index" :value="warehouse">
-          {{ warehouse }}
+        <option value="" disabled>Select Warehouse</option>
+        <option v-for="warehouse in warehouseOptions" :key="warehouse.id" :value="warehouse.id">
+          {{ warehouse.warehouse }}
         </option>
       </select>
       <p v-if="warehouseEmpty" class="text-danger"> The warehouse can't be empty!</p>
@@ -32,6 +34,8 @@
 <script>
 export default {
   name: "UpdateTeamModal",
+  inject: ['teamsService'],
+  props: ["item"],
   data() {
     return {
       modalItem: {
@@ -40,35 +44,48 @@ export default {
       },
       teamEmpty: false,
       warehouseEmpty: false,
-      warehouseOptions: ["SolarSedum", "Superzon", "Theswitch", "Induct", "EHES"]
-    }
+      warehouseOptions: [],
+    };
   },
 
-  props: ["item"],
   created() {
-    this.modalItem = Object.assign({}, this.item)
+    this.modalItem = Object.assign({}, this.item);
+    this.fetchWarehouseOptions();
+    this.setDefaultWarehouse();
   },
 
   computed: {
     hasError() {
       return this.teamEmpty || this.warehouseEmpty;
-    }
+    },
   },
 
   methods: {
+    async fetchWarehouseOptions() {
+      try {
+        const data = await this.teamsService.getTeamModalData();
+        this.warehouseOptions = data.warehouses;
+      } catch (error) {
+        console.error("Error fetching warehouse options:", error);
+      }
+    },
+
     validateTeam() {
       this.teamEmpty = this.modalItem.team.length === 0;
       this.hasError = this.modalItem.team.length === 0;
     },
 
     validateWarehouse() {
-      this.warehouseEmpty = this.modalItem.warehouse.length === 0;
-      this.hasError = this.modalItem.warehouse.length === 0;
-    }
-  }
-}
-</script>
+      this.warehouseEmpty = !this.modalItem.warehouse;
+      this.hasError = !this.modalItem.warehouse;
+    },
 
+    setDefaultWarehouse() {
+      this.modalItem.warehouse = this.modalItem.warehouse?.id || "";
+    },
+  },
+};
+</script>
 
 <style scoped>
 .form-control:disabled {
