c6bc4c49145db9d9c99d291dceb52c850cc2786e
Tim Knops <timknops22@gmail.com>
2024-01-04 12:15:03 +0100

    Adding formatting tests and working on update project test

--- front-end/tests/unit/components/projectsOverview.spec.js
+++ front-end/tests/unit/components/projectsOverview.spec.js
@@ -4,6 +4,56 @@ import { ProjectAdaptor } from "@/service/projectAdaptor";
 
 jest.mock("@/service/projectAdaptor");
 
+const mockProjects = [
+  {
+    id: 1,
+    projectName: "Project 55",
+    team: {
+      id: 1,
+      team: "Team 307",
+    },
+    client: "Client 13",
+    dueDate: "2025-02-18",
+    description:
+      "Solar Sedum is proud to announce the successful completion of Project GreenSky, where we transformed urban rooftops into vibrant, eco-friendly spaces.",
+    status: "IN_PROGRESS",
+  },
+  {
+    id: 2,
+    projectName: "Project 50",
+    team: {
+      id: 5,
+      team: "Team 377",
+    },
+    client: "Client 3",
+    dueDate: "2023-08-02",
+    description:
+      "Solar Sedum is proud to announce the successful completion of Project GreenSky, where we transformed urban rooftops into vibrant, eco-friendly spaces.",
+    status: "COMPLETED",
+  },
+  {
+    id: 3,
+    projectName: "Project 23",
+    team: {
+      id: 6,
+      team: "Team 984",
+    },
+    client: "Client 19",
+    dueDate: "2025-03-12",
+    description:
+      "Superzon is a project that aims to provide solar energy to the entire city of Amsterdam. We are proud to announce that we have successfully completed the first phase of this project.",
+    status: "UPCOMING",
+  },
+];
+
+/**
+ * Unit tests for ProjectsOverview component.
+ *
+ * @test {ProjectsOverview}
+ * @group unit/components
+ * @groupname projectsOverview
+ * @author Tim Knops
+ */
 describe("ProjectsOverview", () => {
   let wrapper;
 
@@ -11,50 +61,11 @@ describe("ProjectsOverview", () => {
     // Mock the project adaptor.
     ProjectAdaptor.mockImplementation(() => {
       return {
-        getAll: jest.fn().mockResolvedValue([
-          {
-            id: 1,
-            projectName: "Project 55",
-            team: {
-              id: 1,
-              team: "Team 307",
-            },
-            client: "Client 13",
-            dueDate: "2025-02-18",
-            description:
-              "Solar Sedum is proud to announce the successful completion of Project GreenSky, where we transformed urban rooftops into vibrant, eco-friendly spaces.",
-            status: "IN_PROGRESS",
-          },
-          {
-            id: 2,
-            projectName: "Project 50",
-            team: {
-              id: 5,
-              team: "Team 377",
-            },
-            client: "Client 3",
-            dueDate: "2023-08-02",
-            description:
-              "Solar Sedum is proud to announce the successful completion of Project GreenSky, where we transformed urban rooftops into vibrant, eco-friendly spaces.",
-            status: "COMPLETED",
-          },
-          {
-            id: 3,
-            projectName: "Project 23",
-            team: {
-              id: 6,
-              team: "Team 984",
-            },
-            client: "Client 19",
-            dueDate: "2025-03-12",
-            description:
-              "Superzon is a project that aims to provide solar energy to the entire city of Amsterdam. We are proud to announce that we have successfully completed the first phase of this project.",
-            status: "UPCOMING",
-          },
-        ]),
+        getAll: jest.fn().mockResolvedValue(mockProjects),
       };
     });
 
+    // Mount the component.
     wrapper = shallowMount(ProjectsOverview, {
       data() {
         return {
@@ -68,38 +79,9 @@ describe("ProjectsOverview", () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  it("initializes data correctly", () => {
-    expect(wrapper.vm.projects).toEqual([]);
-    expect(wrapper.vm.projectsAreLoading).toBe(true);
-    expect(wrapper.vm.showModal).toBe(false);
-    expect(wrapper.vm.modalTitle).toBe("");
-    expect(wrapper.vm.modalBodyComponent).toBe("");
-    expect(wrapper.vm.modalProject).toEqual({
-      id: Number,
-      projectName: String,
-      client: String,
-      dueDate: Date,
-      team: String,
-      status: String,
-    });
-    expect(wrapper.vm.okBtnText).toBe("");
-    expect(wrapper.vm.showToast).toBe(false);
-    expect(wrapper.vm.toastTitle).toBe("");
-    expect(wrapper.vm.toastMessage).toBe("");
-  });
-
-  it("fetches projects on created lifecycle hook", async () => {
-    const mockProjectService = {
-      getAll: jest.fn().mockResolvedValue([]),
-    };
-    wrapper.setData({ projectService: mockProjectService });
-
-    await wrapper.vm.created();
-
-    expect(mockProjectService.getAll).toHaveBeenCalled();
-    expect(wrapper.vm.projects).toEqual([wrapper.vm.formatEmptyTableData()]);
-    expect(wrapper.vm.projectsAreLoading).toBe(false);
-  });
+  /**
+   * Data formatting tests.
+   */
 
   it("formats project correctly for table", () => {
     const project = {
@@ -119,17 +101,47 @@ describe("ProjectsOverview", () => {
       client: "Client 1",
       dueDate: "Dec 31, 2023",
       team: "Team 1",
+      status: "In Progress",
+    });
+  });
+
+  it("should format the project for the table", () => {
+    const project = {
+      id: 1,
+      projectName: "Project 1",
+      team: {
+        id: 1,
+        team: "Team 1",
+      },
+      client: "Client 1",
+      dueDate: "2023-12-31",
+      description: "Project Description",
+      status: "IN_PROGRESS",
+      products: [
+        { product_id: 1, quantity: 500 },
+        { product_id: 2, quantity: 300 },
+      ],
+    };
+
+    const formattedProject = wrapper.vm.formatProjectForTable(project);
+
+    expect(formattedProject).toEqual({
+      id: 1,
+      name: "Project 1",
+      client: "Client 1",
+      dueDate: "Dec 31, 2023",
+      team: "Team 1",
       status: "IN_PROGRESS",
     });
   });
 
-  it("formats project correctly for request", () => {
+  it("should format the project for the request", () => {
     const project = {
       id: 1,
       projectName: "Project 1",
       team: 1,
       client: "Client 1",
-      dueDate: "2023-12-31",
+      dueDate: "2022-12-31",
       description: "Project Description",
       status: "In Progress",
       products: [
@@ -146,7 +158,7 @@ describe("ProjectsOverview", () => {
         projectName: "Project 1",
         team: { id: 1 },
         client: "Client 1",
-        dueDate: "2023-12-31",
+        dueDate: "2022-12-31",
         description: "Project Description",
         status: "IN_PROGRESS",
       },
@@ -155,5 +167,143 @@ describe("ProjectsOverview", () => {
         { product: { id: 2 }, quantity: 300 },
       ],
     });
+  });
+
+  it("should filter out products with empty product_id", () => {
+    const project = {
+      id: 1,
+      projectName: "Project 1",
+      team: 1,
+      client: "Client 1",
+      dueDate: "2022-12-31",
+      description: "Project Description",
+      status: "In Progress",
+      products: [
+        { product_id: 1, quantity: 500 },
+        { product_id: "", quantity: 300 },
+      ],
+    };
+
+    const formattedProject = wrapper.vm.formatProjectForRequest(project);
+
+    expect(formattedProject).toEqual({
+      project: {
+        id: 1,
+        projectName: "Project 1",
+        team: { id: 1 },
+        client: "Client 1",
+        dueDate: "2022-12-31",
+        description: "Project Description",
+        status: "IN_PROGRESS",
+      },
+      resources: [{ product: { id: 1 }, quantity: 500 }],
+    });
+  });
+
+  it("should format the date", () => {
+    const date = new Date("2022-06-01");
+    const formattedDate = wrapper.vm.formatDate(date);
+
+    expect(formattedDate).toBe("Jun 1, 2022");
+  });
+
+  it("should return empty table data", () => {
+    const emptyTableData = wrapper.vm.formatEmptyTableData();
+
+    expect(emptyTableData).toEqual({
+      id: "",
+      name: "",
+      client: "",
+      dueDate: "",
+      team: "",
+      status: "",
+    });
+  });
+
+  it("should update a project when updateProject method is called", async () => {
+    // Mock the projectService
+    const mockProjectService = {
+      update: jest
+        .fn()
+        .mockResolvedValue({ id: 1, projectName: "Updated Project" }),
+    };
+    wrapper.vm.projectService = mockProjectService;
+
+    // Set up the initial project data
+    const initialProject = {
+      id: 1,
+      projectName: "Project",
+      client: "Client",
+      dueDate: "2023-12-31",
+      team: "Team",
+      status: "UPCOMING",
+    };
+    wrapper.setData({ projects: [initialProject] });
+
+    // Call the updateProject method.
+    await wrapper.vm.updateProject(initialProject);
+
+    // Wait for Vue to update the DOM.
+    await wrapper.vm.$nextTick();
+
+    // Check if the projectService update method was called with the correct arguments.
+    expect(mockProjectService.update).toHaveBeenCalledWith({
+      project: {
+        id: 1,
+        projectName: "Project",
+        team: { id: 1 },
+        client: "Client",
+        dueDate: "2023-12-31",
+        description: undefined,
+        status: "UPCOMING",
+      },
+      resources: [],
+    });
+
+    // Check if the project was updated in the projects array.
+    expect(wrapper.vm.projects[0].projectName).toBe("Updated Project");
+  });
+
+  it("should show an error toast when updateProject fails", async () => {
+    // Mock the projectService to throw an error.
+    const mockProjectService = {
+      update: jest
+        .fn()
+        .mockRejectedValue(new Error("Failed to update project")),
+    };
+    wrapper.vm.projectService = mockProjectService;
+
+    // Set up the initial project data
+    const initialProject = {
+      id: 1,
+      projectName: "Project",
+      client: "Client",
+      dueDate: "2023-12-31",
+      team: "Team",
+      status: "UPCOMING",
+    };
+    wrapper.setData({ projects: [initialProject] });
+
+    // Call the updateProject method
+    await wrapper.vm.updateProject(initialProject);
+
+    // Check if the projectService update method was called with the correct arguments
+    expect(mockProjectService.update).toHaveBeenCalledWith({
+      project: {
+        id: 1,
+        projectName: "Project",
+        team: { id: "Team" },
+        client: "Client",
+        dueDate: "2023-12-31",
+        description: undefined,
+        status: "UPCOMING",
+      },
+      resources: [],
+    });
+
+    // Check if the error toast is shown
+    expect(wrapper.vm.showToast).toBe(true);
+    expect(wrapper.vm.toastTitle).toBe("Failed to update");
+    expect(wrapper.vm.toastMessage).toBe("Failed to update project");
   });
 });
