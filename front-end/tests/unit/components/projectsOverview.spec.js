b7a7e9a4468c669b016b1cb12ef420e5235e86a0
Tim Knops <timknops22@gmail.com>
2024-01-05 11:18:52 +0100

    Finishing the projectsOverview component unit tests

--- front-end/tests/unit/components/projectsOverview.spec.js
+++ front-end/tests/unit/components/projectsOverview.spec.js
@@ -57,20 +57,30 @@ const mockProjects = [
 describe("ProjectsOverview", () => {
   let wrapper;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     // Mock the project adaptor.
     ProjectAdaptor.mockImplementation(() => {
       return {
         getAll: jest.fn().mockResolvedValue(mockProjects),
+        update: jest.fn().mockResolvedValue(mockProjects[0]),
+        delete: jest.fn().mockResolvedValue(mockProjects[0]),
       };
     });
 
+    // Mock the router.
+    const $router = {
+      push: jest.fn(),
+    };
+
     // Mount the component.
     wrapper = shallowMount(ProjectsOverview, {
-      data() {
-        return {
+      global: {
+        provide: {
           projectService: new ProjectAdaptor(),
-        };
+        },
+        mocks: {
+          $router,
+        },
       },
     });
   });
@@ -82,28 +92,6 @@ describe("ProjectsOverview", () => {
   /**
    * Data formatting tests.
    */
-
-  it("formats project correctly for table", () => {
-    const project = {
-      id: 1,
-      projectName: "Project 1",
-      client: "Client 1",
-      dueDate: "2023-12-31",
-      team: { team: "Team 1" },
-      status: "In Progress",
-    };
-
-    const formattedProject = wrapper.vm.formatProjectForTable(project);
-
-    expect(formattedProject).toEqual({
-      id: 1,
-      name: "Project 1",
-      client: "Client 1",
-      dueDate: "Dec 31, 2023",
-      team: "Team 1",
-      status: "In Progress",
-    });
-  });
 
   it("should format the project for the table", () => {
     const project = {
@@ -220,90 +208,149 @@ describe("ProjectsOverview", () => {
     });
   });
 
-  it("should update a project when updateProject method is called", async () => {
-    // Mock the projectService
-    const mockProjectService = {
-      update: jest
-        .fn()
-        .mockResolvedValue({ id: 1, projectName: "Updated Project" }),
-    };
-    wrapper.vm.projectService = mockProjectService;
+  /**
+   * Update/delete tests.
+   */
 
-    // Set up the initial project data
-    const initialProject = {
-      id: 1,
-      projectName: "Project",
-      client: "Client",
-      dueDate: "2023-12-31",
-      team: "Team",
-      status: "UPCOMING",
-    };
-    wrapper.setData({ projects: [initialProject] });
+  it("calls projectService.getAll() on created lifecycle hook", async () => {
+    // Mock the getAll method.
+    const getAllSpy = jest.spyOn(wrapper.vm.projectService, "getAll");
 
-    // Call the updateProject method.
-    await wrapper.vm.updateProject(initialProject);
+    // If the component has a created hook, it will be executed.
+    if (wrapper.vm.$options.created && wrapper.vm.$options.created.length > 0) {
+      await wrapper.vm.$options.created[0].call(wrapper.vm); // Call the created lifecycle hook directly
+    }
 
-    // Wait for Vue to update the DOM.
-    await wrapper.vm.$nextTick();
+    // Check if the getAll method was called.
+    expect(getAllSpy).toHaveBeenCalled();
 
-    // Check if the projectService update method was called with the correct arguments.
-    expect(mockProjectService.update).toHaveBeenCalledWith({
-      project: {
-        id: 1,
-        projectName: "Project",
-        team: { id: 1 },
-        client: "Client",
-        dueDate: "2023-12-31",
-        description: undefined,
-        status: "UPCOMING",
-      },
-      resources: [],
-    });
-
-    // Check if the project was updated in the projects array.
-    expect(wrapper.vm.projects[0].projectName).toBe("Updated Project");
+    // Restore the spy.
+    getAllSpy.mockRestore();
   });
 
-  it("should show an error toast when updateProject fails", async () => {
-    // Mock the projectService to throw an error.
-    const mockProjectService = {
-      update: jest
-        .fn()
-        .mockRejectedValue(new Error("Failed to update project")),
-    };
-    wrapper.vm.projectService = mockProjectService;
+  it("calls projectService.delete() and updates projects array when deleteProject method is called", async () => {
+    const project = { id: 1 };
 
-    // Set up the initial project data
-    const initialProject = {
-      id: 1,
-      projectName: "Project",
-      client: "Client",
-      dueDate: "2023-12-31",
-      team: "Team",
-      status: "UPCOMING",
-    };
-    wrapper.setData({ projects: [initialProject] });
+    // Mock the delete method.
+    const deleteSpy = jest.spyOn(wrapper.vm.projectService, "delete");
 
-    // Call the updateProject method
-    await wrapper.vm.updateProject(initialProject);
+    // Call the method.
+    await wrapper.vm.deleteProject(project);
 
-    // Check if the projectService update method was called with the correct arguments
-    expect(mockProjectService.update).toHaveBeenCalledWith({
-      project: {
-        id: 1,
-        projectName: "Project",
-        team: { id: "Team" },
-        client: "Client",
-        dueDate: "2023-12-31",
-        description: undefined,
-        status: "UPCOMING",
+    // Check if the delete method was called with the correct project.
+    expect(deleteSpy).toHaveBeenCalledWith(1);
+
+    // Check if the projects array was updated.
+    expect(wrapper.vm.projects).toEqual([
+      {
+        client: "Client 3",
+        dueDate: "Aug 2, 2023",
+        id: 2,
+        name: "Project 50",
+        status: "COMPLETED",
+        team: "Team 377",
       },
-      resources: [],
-    });
+      {
+        client: "Client 19",
+        dueDate: "Mar 12, 2025",
+        id: 3,
+        name: "Project 23",
+        status: "UPCOMING",
+        team: "Team 984",
+      },
+    ]);
 
-    // Check if the error toast is shown
+    // Restore the spy.
+    deleteSpy.mockRestore();
+  });
+
+  it("calls projectService.update() and updates projects array when updateProject method is called", async () => {
+    const project = { id: 1, team: {} };
+
+    // Mock the update method.
+    const updateSpy = jest.spyOn(wrapper.vm.projectService, "update");
+
+    // Call the method.
+    wrapper.vm.formatProjectForRequest = jest.fn().mockReturnValue(project);
+    await wrapper.vm.updateProject(project);
+
+    // Check if the update method was called with the correct project.
+    expect(updateSpy).toHaveBeenCalledWith(project);
+
+    // Check if the projects array was updated.
+    expect(wrapper.vm.projects).toEqual([
+      {
+        client: "Client 13",
+        dueDate: "Feb 18, 2025",
+        id: 1,
+        name: "Project 55",
+        status: "IN_PROGRESS",
+        team: "Team 307",
+      },
+      {
+        client: "Client 3",
+        dueDate: "Aug 2, 2023",
+        id: 2,
+        name: "Project 50",
+        status: "COMPLETED",
+        team: "Team 377",
+      },
+      {
+        client: "Client 19",
+        dueDate: "Mar 12, 2025",
+        id: 3,
+        name: "Project 23",
+        status: "UPCOMING",
+        team: "Team 984",
+      },
+    ]);
+
+    // Restore the spy.
+    updateSpy.mockRestore();
+  });
+
+  /**
+   * Toast test.
+   */
+
+  it("shows the toast and hides it after a certain duration when showTimedToast method is called", () => {
+    jest.useFakeTimers(); // Mock the timers.
+    const title = "Success";
+    const message = "Project added.";
+
+    // Call the method.
+    wrapper.vm.showTimedToast(title, message, 4000);
+
+    // Check if the toast is shown and the title and message are correct.
+    expect(wrapper.vm.toastTitle).toBe(title);
+    expect(wrapper.vm.toastMessage).toBe(message);
     expect(wrapper.vm.showToast).toBe(true);
-    expect(wrapper.vm.toastTitle).toBe("Failed to update");
-    expect(wrapper.vm.toastMessage).toBe("Failed to update project");
+
+    // Advance the timers by 4 seconds.
+    jest.advanceTimersByTime(4000);
+
+    // Check if the toast is hidden.
+    expect(wrapper.vm.showToast).toBe(false);
+
+    // Restore the timers.
+    jest.useRealTimers();
+  });
+
+  /**
+   * Navigation test.
+   */
+
+  it("navigates to specific view when showSpecificView method is called", () => {
+    const project = { id: 1 };
+    const pushSpy = jest.spyOn(wrapper.vm.$router, "push");
+
+    // Call the method.
+    wrapper.vm.showSpecificView(project);
+
+    // Check if the router was called with the correct path.
+    expect(pushSpy).toHaveBeenCalledWith(`/projects/${project.id}`);
+
+    // Restore the spy.
+    pushSpy.mockRestore();
   });
 });
