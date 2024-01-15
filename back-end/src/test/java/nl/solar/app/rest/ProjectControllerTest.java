26051b5186becc8263804e61a781218b43efe35a
Tim Knops <timknops22@gmail.com>
2024-01-15 11:40:33 +0100

    Adding tests for further CRUD operations of projects

--- back-end/src/test/java/nl/solar/app/rest/ProjectControllerTest.java
+++ back-end/src/test/java/nl/solar/app/rest/ProjectControllerTest.java
@@ -1,16 +1,12 @@
 
 package nl.solar.app.rest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import nl.solar.app.WebConfig;
+import nl.solar.app.exceptions.PreConditionFailedException;
+import nl.solar.app.exceptions.ResourceNotFoundException;
 import nl.solar.app.models.wrappers.ProjectRequestWrapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +26,9 @@ import nl.solar.app.models.Project;
 import nl.solar.app.models.Team;
 import nl.solar.app.repositories.ProjectRepository;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.HttpClientErrorException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ProjectControllerTest {
@@ -53,7 +53,6 @@ public class ProjectControllerTest {
 
     @Test
     public void shouldReturnAllProjectsWhenGetAllIsCalled() {
-        // Send HTTP GET request to the API endpoint
         ResponseEntity<List<Project>> response = restTemplate.exchange("/projects", HttpMethod.GET, null,
                 new ParameterizedTypeReference<>() {
                 });
@@ -63,5 +62,115 @@ public class ProjectControllerTest {
 
         // Check if the body is not empty.
         assertNotNull(response.getBody());
+    }
+
+    @Test
+    public void shouldDeleteProjectWhenCalledWithValidId() {
+        ResponseEntity<Project> response = restTemplate.exchange("/projects/1", HttpMethod.DELETE, null,
+                new ParameterizedTypeReference<>() {
+                });
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    public void shouldReturnErrorWhenGetProjectIsCalledWithInvalidId() {
+        ResponseEntity<Map<String, Object>> response = restTemplate.exchange("/projects/9999", HttpMethod.GET, null,
+                new ParameterizedTypeReference<>() {
+                });
+
+        // Response should be OK, because of front-end handling.
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        // Check if the body contains an error.
+        assertTrue(Objects.requireNonNull(response.getBody()).containsKey("error"));
+    }
+
+    @Test
+    public void shouldReturnProjectsWhenGetProjectsForTeamIsCalledWithValidTeamId() {
+        ResponseEntity<List<Project>> response = restTemplate.exchange("/projects/team/1", HttpMethod.GET, null,
+                new ParameterizedTypeReference<>() {
+                });
+
+        // Check if the response is OK.
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        // Check if the body is not empty.
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    public void shouldCreateProjectWhenValidRequestIsGiven() {
+        // Get a team.
+        ResponseEntity<List<Team>> teamResponse = restTemplate.exchange("/teams", HttpMethod.GET, null,
+                new ParameterizedTypeReference<>() {
+                });
+
+        if (Objects.requireNonNull(teamResponse.getBody()).isEmpty()) {
+            fail("No teams found");
+        }
+
+        // Create a project.
+        Project project = new Project();
+        project.setProjectName("Test project");
+        project.setClient("Test client");
+        project.setDueDate(new Date());
+        project.setStatus(ProjectStatus.IN_PROGRESS);
+        project.setTeam(teamResponse.getBody().get(0));
+
+        // Create a new projectWrapper, used for the request.
+        ProjectRequestWrapper projectWrapper = new ProjectRequestWrapper(project, null);
+
+        // Create a request.
+        HttpEntity<ProjectRequestWrapper> request = new HttpEntity<>(projectWrapper);
+
+        // Send the request.
+        ResponseEntity<Project> response = restTemplate.exchange("/projects", HttpMethod.POST, request,
+                new ParameterizedTypeReference<>() {
+                });
+
+        // Check if the response is OK.
+        Project createdProject = response.getBody();
+
+        // Assert that the project is not null.
+        assert createdProject != null;
+
+        // Assertions for the created project.
+        assertEquals("Test project", createdProject.getProjectName());
+        assertEquals("Test client", createdProject.getClient());
+        assertEquals(ProjectStatus.IN_PROGRESS, createdProject.getStatus());
+        assertEquals(teamResponse.getBody().get(0).getId(), createdProject.getTeam().getId());
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    public void shouldReturnErrorWhenCreateProjectIsCalledWithInvalidTeamId() {
+        // Create a project.
+        Project project = new Project();
+        project.setProjectName("Test project");
+        project.setClient("Test client");
+        project.setDueDate(new Date());
+        project.setStatus(ProjectStatus.IN_PROGRESS);
+        project.setTeam(new Team());
+
+        // Create a new projectWrapper, used for the request.
+        ProjectRequestWrapper projectWrapper = new ProjectRequestWrapper(project, null);
+
+        // Create a request.
+        HttpEntity<ProjectRequestWrapper> request = new HttpEntity<>(projectWrapper);
+
+        // Send the request.
+        ResponseEntity<Map<String, Object>> response = restTemplate.exchange("/projects", HttpMethod.POST, request,
+                new ParameterizedTypeReference<>() {
+                });
+
+        // Check if the response is not found.
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+
+        // Check if the body contains an error.
+        assertTrue(Objects.requireNonNull(response.getBody()).containsKey("error"));
     }
 }
