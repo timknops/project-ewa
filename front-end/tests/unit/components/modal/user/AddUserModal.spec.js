64e5226d6f8275ef814c9b8ab3bc52d94edb3060
Noa de Greef <noa.de.greef@hva.nl>
2024-01-16 02:15:27 +0100

    User overview testing mostly done, need to look more at the standards though - add user modal test start - also started login page start

---
+++ front-end/tests/unit/components/modal/user/AddUserModal.spec.js
@@ -0,0 +1,44 @@
+import {shallowMount} from "@vue/test-utils";
+import AddUserModal from "@/components/modal/user/AddUserModal.vue";
+import {TeamAdaptor} from "@/service/teamAdaptor";
+
+const teams = {
+    id: 1,
+    teamName: "Solar Sedum",
+    warehouse: "Warehouse 1",
+    type: "INTERNAL"
+}
+
+const users = {
+    name: "admin2",
+    email: "admin2@admin.com",
+    password: "admin2",
+    type: "ADMIN",
+    team: teams[0]
+}
+
+describe('AddUserModal.vue', () => {
+    const mockTeamAdaptor = new TeamAdaptor();
+    jest.spyOn(mockTeamAdaptor, 'findAll').mockResolvedValue([
+        {
+            teams
+        }])
+
+
+    let wrapper;
+    beforeEach(async () => {
+        wrapper = shallowMount(AddUserModal, {
+            global: {
+                provide: {
+                    teamsService: mockTeamAdaptor
+                }
+            }
+        })
+        console.log(users)
+        await wrapper.vm.$nextTick();
+    })
+
+    it('Add modal is rendered', () => {
+        expect(wrapper.element.children.length, "Add modal should be rendered").toBeGreaterThan(0)
+    })
+})
